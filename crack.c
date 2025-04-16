ca52e5e1501b9abeed5ac377a9a6a08b2c810a98
Crystal Duran <cduran3@student.sierracollege.edu>
2025-04-16 14:32:06 -0700

    changes

--- crack.c
+++ crack.c
@@ -11,38 +11,55 @@
 #define PASS_LEN 50     // Maximum length any password will be.
 #define HASH_LEN 33     // Length of hash plus one for null.
 
-
-int main(int argc, char *argv[])
-{
-    if (argc < 3) 
-    {
+int main(int argc, char *argv[]) {
+    if (argc < 3) {
         printf("Usage: %s hash_file dictionary_file\n", argv[0]);
         exit(1);
     }
 
-    // TODO: Read the hashes file into an array.
-    //   Use either a 2D array or an array of arrays.
-    //   Use the loadFile function from fileutil.c
-    //   Uncomment the appropriate statement.
+    // Loads hashes file into an array using loadFileAA
     int size;
-    //char (*hashes)[HASH_LEN] = loadFile(argv[1], &size);
-    //char **hashes = loadFile(argv[1], &size);
-    
-    // CHALLENGE1: Sort the hashes using qsort.
-    
-    // TODO
-    // Open the password file for reading.
+    char **hashes = loadFileAA(argv[1], &size);
 
-    // TODO
-    // For each password, hash it, then use the array search
-    // function from fileutil.h to find the hash.
-    // If you find it, display the password and the hash.
-    // Keep track of how many hashes were found.
-    // CHALLENGE1: Use binary search instead of linear search.
+    // Opens file
+    FILE *passFile = fopen(argv[2], "r");
+    
+    if (!passFile) {
+        perror("Failed to open dictionary file");
+        freeAA(hashes, size);
+        exit(1);
+    }
 
-    // TODO
-    // When done with the file:
-    //   Close the file
-    //   Display the number of hashes found.
-    //   Free up memory.
+    char word[PASS_LEN];
+    int crackedCount = 0;
+
+    // Reads each password 
+    while (fgets(word, PASS_LEN, passFile)) {
+        word[strcspn(word, "\n")] = 0;
+
+        // Hashes word
+        unsigned char hashOutput[16];
+        MD5((unsigned char*)word, strlen(word), hashOutput);
+
+        char hashString[HASH_LEN];
+
+        for (int i = 0; i < 16; i++) {
+            sprintf(&hashString[i * 2], "%02x", (unsigned int)hashOutput[i]);
+        }
+
+        // Linear searches
+        int foundIndex = linearSearch(hashes, size, hashString);
+
+        if (foundIndex != -1) {
+            printf("%s %s\n", hashString, word);
+            crackedCount++;
+        }
+    }
+    
+    // Closes the file , frees memory, and displays number of hashes found
+    fclose(passFile);
+    freeAA(hashes, size);
+    printf("%d hashes cracked!\n", crackedCount);
+    return 0;
 }
+
